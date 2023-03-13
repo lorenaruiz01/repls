f8a9c89d851129a7a98cbe19874c854556fc9195
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-13 10:33:31 -0700

    Add files via upload

---
+++ repls/1.6.1_DOMNodeTraversal/script.js
@@ -0,0 +1,12 @@
+
+let element = document.querySelector('.bob');
+
+//uncomment the lines one by one to check the console output. Make sure only one line is uncommented at one time
+
+//console.log(element.nextElementSibling.innerText); // Get the next sibling element, or null if it’s the last one
+//console.log(element.previousElementSibling.innerText); // Get the previous sibling element, or null if it’s the first one
+//console.log(element.firstElementChild); // The first child of the element
+//console.log(element.lastElementChild); // The last child of the element
+//console.log(element.childElementCount); // The number of children this element has
+
+
