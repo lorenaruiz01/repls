4e9969cd8eac32da72a27da684e201360fb01451
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-14 21:09:19 -0700

    add removeChild repl

---
+++ repls/1.6.6_removeChild/script.js
@@ -0,0 +1,4 @@
+// Uncomment the following lines to remove the paragraph element
+
+// let elementToRemove = document.querySelector('p');
+// elementToRemove.parentElement.removeChild(elementToRemove);
