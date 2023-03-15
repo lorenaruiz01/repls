27ab3f9d3e24ee8850ce43563a032966cc7fcaaa
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-14 19:51:38 -0700

    add repl 1.6.4_appendChild()

---
+++ repls/1.6.4_appendChild/script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('.container');
+
+//uncomment the following lines
+
+let button = document.createElement('button');
+button.innerText = "Test";
+container.appendChild(button);
