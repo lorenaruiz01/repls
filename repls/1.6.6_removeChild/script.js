1733721526602de64c62e0f1b4a270f622d874d5
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-14 21:11:16 -0700

    uncomment remove function

--- repls/1.6.6_removeChild/script.js
+++ repls/1.6.6_removeChild/script.js
@@ -1,4 +1,4 @@
 // Uncomment the following lines to remove the paragraph element
 
-// let elementToRemove = document.querySelector('p');
-// elementToRemove.parentElement.removeChild(elementToRemove);
+let elementToRemove = document.querySelector('p');
+elementToRemove.parentElement.removeChild(elementToRemove);
