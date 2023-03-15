acbef8b14b9747848b6cf90fca84a53cffb9610e
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-14 21:12:05 -0700

    add commen re: removeChild() function

--- repls/1.6.6_removeChild/script.js
+++ repls/1.6.6_removeChild/script.js
@@ -1,3 +1,7 @@
+// Take a look at the code below, where the removeChild() function is being used to remove the <p> element from the HTML code section:
+
+
+
 // Uncomment the following lines to remove the paragraph element
 
 let elementToRemove = document.querySelector('p');
