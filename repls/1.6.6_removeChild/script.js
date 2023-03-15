369052fbc99266701842cb65df43fe07a9cca724
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-14 21:15:51 -0700

    add step by step explanation for removeChild() fxn

--- repls/1.6.6_removeChild/script.js
+++ repls/1.6.6_removeChild/script.js
@@ -6,3 +6,7 @@
 
 let elementToRemove = document.querySelector('p');
 elementToRemove.parentElement.removeChild(elementToRemove);
+
+// You’ll notice that using the removeChild() function is a bit more complicated than the three functions you learned before: this is because you can’t simply remove a node (the function is removeChild() rather than simply remove). Let’s walk through it step-by-step.
+
+// First, you select the element you want to remove (assigned to elementToRemove above). Then, you traverse up to select its parent element. Finally, you call the removeChild() function on the parent element to remove its child (which is the element you originally selected). Complicated? Unfortunately, yes. This is just one of the many quirks of JavaScript. Don’t worry, though—throughout this course, you’ll learn ways of simplifying these kinds of actions.
