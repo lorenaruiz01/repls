031404c0912277921e363564851cb4236ea14200
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-14 20:33:43 -0700

    add moveElement repl

---
+++ repls/1.6.5_moveElement/script.js
@@ -0,0 +1,5 @@
+let paragraph = document.querySelector('p');
+let container2 = document.querySelector('#container2');
+
+//Uncomment the following line to move the paragraph using appendChild method
+container2.appendChild(paragraph);
