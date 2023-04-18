c8c0daf22300a48a3f04d2e0ff8d8d0195b7e651
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-18 14:51:00 -0700

    script.js

---
+++ repls/1.8.1_Creating-UI-Patterns-Form-Validation/script.js
@@ -0,0 +1,28 @@
+(function() {
+  let form = document.querySelector('#register-form');
+  let emailInput = document.querySelector('#email');
+  let passwordInput = document.querySelector('#password');
+  
+  function validateEmail() {
+    
+    return false;
+  }
+  
+  function validatePassword() {
+    
+    return false;
+  }
+  
+  function validateForm() {
+    return validateEmail() && validatePassword();
+  }
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Do not submit to the server
+    if (validateForm()) {
+      alert('Success!');
+    }
+  })
+
+  // THE RETURN STATEMENT HERE
+})();
