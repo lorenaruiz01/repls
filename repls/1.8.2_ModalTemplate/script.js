cb89aa3de0c2fd09e090ce828e8eb4e6cd61800a
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-09 08:19:33 -0700

    addEventListener to run showModal fxn

--- repls/1.8.2_ModalTemplate/script.js
+++ repls/1.8.2_ModalTemplate/script.js
@@ -2,3 +2,7 @@ function showModal () {
     let modalContainer = document.querySelector ('#modal-container');
     modalContainer.classList.add('is-visible');
 }
+
+document.querySelector ('#show-modal').addEventListener('click', () => {
+    showModal();
+});
