b0b97433b060934fa64badae91b9672ee15fcb7e
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-10 13:36:55 -0700

    edit order of instructions

--- repls/1.8.2_ModalTemplate/script.js
+++ repls/1.8.2_ModalTemplate/script.js
@@ -8,7 +8,25 @@ function showModal(title, text) {
     let modal = document.createElement('div');
     modal.classList.add ('modal');
     
-    
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.ELEMENT_NODE.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
     
     
 }
@@ -19,25 +37,7 @@ document.querySelector ('#show-modal').addEventListener('click', () => {
 
 //
 
-// Add the new modal content
-let closeButtonElement = document.createElement('button');
-closeButtonElement.classList.add('modal-close');
-closeButtonElement.innerText = 'Close';
-closeButtonElement.addEventListener('click', hideModal);
 
-
-let titleElement = document.createElement('h1');
-titleElement.innerText = title;
-
-let contentElement = document.createElement('p');
-contentElement.ELEMENT_NODE.innerText = text;
-
-modal.appendChild(closeButtonElement);
-modal.appendChild(titleElement);
-modal.appendChild(contentElement);
-modalContainer.appendChild(modal);
-
-modalContainer.classList.add('is-visible');
 
 
 document.querySelector('#show-modal').addEventListener('click',() => {
