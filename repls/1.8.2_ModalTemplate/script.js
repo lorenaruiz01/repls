7caa49fb64541b1d14448cb096bd1beb7d61ac66
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-10 13:10:45 -0700

    if key pressed is ESC, hide modal

--- repls/1.8.2_ModalTemplate/script.js
+++ repls/1.8.2_ModalTemplate/script.js
@@ -49,6 +49,8 @@ function hideModal(){
 
 window.addEventListener('keydown',(e) => {
     let modalContainer = document.querySelector('#modal-container');
-
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
     }
-)
+
+    });
