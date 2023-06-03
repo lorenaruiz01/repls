796f1e79a17a4fde5c2eb6ce7edb08fa920ee39f
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-06-03 14:07:32 -0700

    practicing the mini tutorial

--- repls/1.9.1_jQuerySelectors/script.js
+++ repls/1.9.1_jQuerySelectors/script.js
@@ -1,5 +1,41 @@
+// ----------------------------
+// Mini Tutorial
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $('input').addClass('red');
 
 // Below, only make the .main-button green
 
 // Below, make the text of all buttons yellow
+
+
+
+
+
