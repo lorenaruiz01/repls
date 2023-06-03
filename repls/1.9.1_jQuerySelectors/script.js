be62599f25549c374ecd256ddfe0ccafff748966
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-06-03 14:27:00 -0700

    append an li to the ol using the list variable

--- repls/1.9.1_jQuerySelectors/script.js
+++ repls/1.9.1_jQuerySelectors/script.js
@@ -16,7 +16,8 @@ let list = $('#list')
 // to append an li element to this list: 
 $('#list').append(<li>four</li>);
 
-
+// to append an li to the ol using the list variable:
+list.append("<li>four</li>");
 
 
 
