38f5d6877b0e5d025d239fb59f1fe636a15f0980
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-09 21:20:33 -0700

    example

--- Achievement 1 repls/1.1.4_HowJavaScriptWorksFilesStructure/script.js
+++ Achievement 1 repls/1.1.4_HowJavaScriptWorksFilesStructure/script.js
@@ -1,4 +1,22 @@
 // alert('Hello world');
 
+
+// ================================================
+
+
+// declare variable and assign value
 let myName = 'Bob';
+// print value to html document
 document.write(myName);
+
+// reassign variable with new value
+myName = 'John Doe';
+// print value to html document
+document.write(myName);
+
+// *this will result in 'BobJohnDoe' being printed to the doc
+
+
+// ================================================
+
+
