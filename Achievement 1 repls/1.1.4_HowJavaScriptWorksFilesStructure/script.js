84b28cbcdb1d0dfd825febe12938f66c949e9226
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-09 21:26:21 -0700

    calculation with parenthesis and mutliple variable

--- Achievement 1 repls/1.1.4_HowJavaScriptWorksFilesStructure/script.js
+++ Achievement 1 repls/1.1.4_HowJavaScriptWorksFilesStructure/script.js
@@ -20,9 +20,14 @@ document.write(myName);
 // ================================================
 
 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition); 
+// let simpleAddition = 2 + 2;
+// document.write(simpleAddition); 
 
-let size = 100; 
-let doubleSize = size * 2;
-document.write(doubleSize);
+// let size = 100; 
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2; 
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize)
