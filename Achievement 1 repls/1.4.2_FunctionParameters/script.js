d1378111a5ba230056f7bda699348f39b4033b0d
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-20 18:15:51 -0700

    script.js

---
+++ Achievement 1 repls/1.4.2_FunctionParameters/script.js
@@ -0,0 +1,7 @@
+function greetingMessage(firstName, lastName) {
+    console.log("Hello, My name is " + firstName + " " + lastName);
+    document.write("Hello , My name is  " + firstName + " " + lastName)
+}
+
+greetingMessage("Brad", "Pitt");
+greetingMessage("Nelson", "Mandela");
