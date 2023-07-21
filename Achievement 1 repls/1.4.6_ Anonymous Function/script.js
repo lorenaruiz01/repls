efd37bfc38b4f2623bef30db51df067d62860be1
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-21 09:41:47 -0700

    script.js

---
+++ Achievement 1 repls/1.4.6_ Anonymous Function/script.js
@@ -0,0 +1,12 @@
+let Message = function (){
+    console.log("Hello World!");
+};
+
+Message();
+
+let sayHello = function (firstName) {
+    console.log("Hello " + firstName);
+};
+
+sayHello("Bobby");
+sayHello();
