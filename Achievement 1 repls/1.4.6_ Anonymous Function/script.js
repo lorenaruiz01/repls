b840ec3fe9451d3959283efbc958c6abaefceefb
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-21 09:56:15 -0700

    comment re: argument passed to parameter

--- Achievement 1 repls/1.4.6_ Anonymous Function/script.js
+++ Achievement 1 repls/1.4.6_ Anonymous Function/script.js
@@ -13,3 +13,10 @@ let sayHello = function (firstName) {
 
 sayHello("Bobby");
 sayHello();
+
+// In the example above, function has one parameter, firstName. When the function is called, The parameter is added to the string hello and logged to the console. 
+
+// The argurment "Bobby" is passed to the function when sayHello variable is called. "Hello Bobby" is then logged to the console. 
+
+// When the sayHello variable is called without any arguments, "Hello undefined" is logged to the console. The code runs and logs "Hello" but since there is no argument passed to the firstName parameter, the program logs, "undefined". There is no default value assigned to firstName which is why the string "firstName" is not logged.
+
