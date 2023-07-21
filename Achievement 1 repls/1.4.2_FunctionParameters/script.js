d233da937b9767e953e1c7358703c0156b8f3185
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-20 19:24:17 -0700

    add line break

--- Achievement 1 repls/1.4.2_FunctionParameters/script.js
+++ Achievement 1 repls/1.4.2_FunctionParameters/script.js
@@ -1,6 +1,6 @@
 function greetingMessage(firstName, lastName) {
     console.log("Hello, My name is " + firstName + " " + lastName);
-    document.write("Hello, My name is  " + firstName + " " + lastName)
+    document.write("Hello, My name is  " + firstName + " " + lastName + '<br>')
 }
 
 greetingMessage("Brad", "Pitt");
