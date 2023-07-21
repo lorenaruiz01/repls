84d9b2c4710cf3ebf3f3af141add8fe5fd92cdad
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-21 08:43:26 -0700

    function with return statement

--- Achievement 1 repls/1.4.4_returnValue/script.js
+++ Achievement 1 repls/1.4.4_returnValue/script.js
@@ -8,3 +8,5 @@ console.log(result);
 // or 
 // document.write(result);
 
+// Here, a function named sum adds val1 and val2 and returns the result. When you execute the function by calling it, it will return the value of adding whatever arguments you’ve chosen for val1 and val2.
+
