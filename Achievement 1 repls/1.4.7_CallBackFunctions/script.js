b785392e9288738f03de4e38321993f9a225d02b
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-21 13:23:19 -0700

    value passed to functionOne and logged to console.

--- Achievement 1 repls/1.4.7_CallBackFunctions/script.js
+++ Achievement 1 repls/1.4.7_CallBackFunctions/script.js
@@ -8,3 +8,8 @@ function functionTwo(variable1) {
 }
 console.log(functionOne(2));
 console.log(functionTwo(functionOne(2)));
+
+// You can chain functions together by using functions as a parameter. Using this method of passing one function’s results to another as parameters is called a callback.
+
+// In the example above, the number 2 gets passed onto the x parameter in functionOne and the number 2 is returned. The return value, 2 in this case, is then logged to the console. 
+
