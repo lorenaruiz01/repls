3fb45d94120a677b9790636797f42cdb9a59baff
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-22 21:50:54 -0700

    printArrayDetails function declaration w/ for loop

--- Achievement 1 repls/1.4.0_TASK/script.js
+++ Achievement 1 repls/1.4.0_TASK/script.js
@@ -1,36 +1,36 @@
 
-// NOTES AND TASK:
+// // NOTES AND TASK:
 
 
 
-let pokemonList = [
-  {
-    name: 'bulbasaur', 
-    height: 0.7, 
-    types: ['grass', 'poison'],
-  }, 
-  {
-    name: 'charizard', 
-    height: 1.7, 
-    types: ['fire', 'flying'],
-  }, 
-  {
-    name: 'squirtle', 
-    height: 1, 
-    types: ['water'],
-  }
-];
+// let pokemonList = [
+//   {
+//     name: 'bulbasaur', 
+//     height: 0.7, 
+//     types: ['grass', 'poison'],
+//   }, 
+//   {
+//     name: 'charizard', 
+//     height: 1.7, 
+//     types: ['fire', 'flying'],
+//   }, 
+//   {
+//     name: 'squirtle', 
+//     height: 1, 
+//     types: ['water'],
+//   }
+// ];
 
-for (let i = 0; i <= pokemonList.length; i++) {
-  if (pokemonList[i].height > 1) {  //check condition of each pokemon height 
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + ' - wow, that\'s a big Pokemon!' + '<br>'); // loops over each pokemon and if true, prints name, height, and message. with line break
-  } else {  
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + '<br>'); // loops over each pokemon and prints name and heigh. with line break
-  }
-};
+// for (let i = 0; i <= pokemonList.length; i++) {
+//   if (pokemonList[i].height > 1) {  //check condition of each pokemon height 
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + ' - wow, that\'s a big Pokemon!' + '<br>'); // loops over each pokemon and if true, prints name, height, and message. with line break
+//   } else {  
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + '<br>'); // loops over each pokemon and prints name and heigh. with line break
+//   }
+// };
 
 
-// Exercise 1.4 changes the name of pokemonList to repository. 
+// Exercise 1.4 changes the name of pokemonList to repository. from pokemonList to repository
 
 // Code with new name for pokemonList: 
 
@@ -52,3 +52,13 @@ let repository = [
   }
 ];
 
+// printArrayDetails function declaration
+function printArrayDetails(){
+  for (let i = 0; i < repository.length; i++){
+    if (pokemonList[i].height > 1) {  //check condition of each pokemon height 
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + ' - wow, that\'s a big Pokemon!' + '<br>'); // loops over each pokemon and if true, prints name, height, and message. with line break
+    } else {  
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + '<br>'); // loops over each pokemon and prints name and heigh. with line break
+    }
+  }
+}
