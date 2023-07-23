21a0d707dbdbc8f22510b63682a1b2763480a1f3
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-22 21:47:01 -0700

    code with new name for pokemonList repository

--- Achievement 1 repls/1.4.0_TASK/script.js
+++ Achievement 1 repls/1.4.0_TASK/script.js
@@ -28,3 +28,27 @@ for (let i = 0; i <= pokemonList.length; i++) {
     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ')' + '<br>'); // loops over each pokemon and prints name and heigh. with line break
   }
 };
+
+
+// Exercise 1.4 changes the name of pokemonList to repository. 
+
+// Code with new name for pokemonList: 
+
+let repository = [
+  {
+    name: 'bulbasaur', 
+    height: 0.7, 
+    types: ['grass', 'poison'],
+  }, 
+  {
+    name: 'charizard', 
+    height: 1.7, 
+    types: ['fire', 'flying'],
+  }, 
+  {
+    name: 'squirtle', 
+    height: 1, 
+    types: ['water'],
+  }
+];
+
