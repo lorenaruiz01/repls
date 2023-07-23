414394725967ea9f9e7e48b869fac9e1b484dd24
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-22 19:58:53 -0700

    script.js

---
+++ Achievement 1 repls/1.4.8_Not-pure-and-Pure-Functions/script.js
@@ -0,0 +1,26 @@
+let age = 10; // value of "age" is 10
+// The function "getNewAgeImpure" is not an example of pure function as the value of "age" changed from 10 to 11 after the execution of the function.
+
+console.log('===Not Pure Function===');
+console.log('Value of "age" before function execution:' + ' ' + age);//returns 10
+function getNewAgeImpure() {
+  age = age + 1;
+  return age;
+}
+getNewAgeImpure();
+console.log('Value of "age" after function execution:' + ' ' + age);//returns 11
+
+// Now, comment out the code from line 4 to line 12
+
+// The function "get" is an example of pure function as the value of "age" changed from 10 to 11 after the execution of the function.
+
+/*
+console.log('===Pure Function===');
+console.log('Value of "age" before function execution:' + ' ' + age);//returns 10
+function getNewAgePure(age) {
+  age = age + 1;
+  return age;
+}
+console.log('The output of "getNewAgePure" function:'+ ' ' + (getNewAgePure(age))); //return 11
+console.log('Value of "age" after function execution:' + ' ' + age); //returns  10
+*/
