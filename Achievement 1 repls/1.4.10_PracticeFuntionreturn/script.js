bf891e96fc28121a5317a91b5ae7b348e1c09f79
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-23 14:59:31 -0700

    divide dividend by divisor and return result

--- Achievement 1 repls/1.4.10_PracticeFuntionreturn/script.js
+++ Achievement 1 repls/1.4.10_PracticeFuntionreturn/script.js
@@ -1,4 +1,10 @@
 function divideAnyValue(dividend, divisor){
     if (divisor === 0) {
         return "you're trying to divide by zero."
-    }  
+    }  else { 
+        let result = dividend / divisor;
+          return result
+      }
+    }
+    
+    
