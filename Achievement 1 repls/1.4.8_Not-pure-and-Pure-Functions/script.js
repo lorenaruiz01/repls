37367cc3b80114d5a49705352a36e21ff5db012e
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-22 20:33:19 -0700

    solve side effect of argument interfering with var

--- Achievement 1 repls/1.4.8_Not-pure-and-Pure-Functions/script.js
+++ Achievement 1 repls/1.4.8_Not-pure-and-Pure-Functions/script.js
@@ -42,3 +42,7 @@ function getNewAge(){
 getNewAge();    // changes the new value of age to 11, then continues adding 1 to the age value. So, 11 + 1 returns 12. Then the new value of age is 12 and getNewAge(); returns 13. Then, the new value of age is 13 and getNewAge(); returns 14, making the new value of age 15. So on and so forth.
 
 console.log(age); // returns the value of age at whatever point age is defined. For example, if getNewAge() returns 15, console.log(age) after running getNewAge() will return 15. This is a side effect.
+
+
+// You can solve this problem (the value of age changing) by passing the variable age as a parameter to the getNewAge() function like below. Now, the statement age = age + 1 in the function block will correspond to the age parameter, rather than interfering with the earlier age variable. 
+
