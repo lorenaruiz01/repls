ca612e3462d7414ef9a25afc42b220d0aa28aa68
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-23 08:23:01 -0700

    change condition to evaluate if higher than 3

--- Achievement 1 repls/1.4.0_TASK/script.js
+++ Achievement 1 repls/1.4.0_TASK/script.js
@@ -137,7 +137,7 @@ let pokemonList2 = [
 
 function printArrayDetails(list){
   for (let i = 0; i < list.length; i++){
-    if (list[i].height > 1) {  //check condition of each pokemon height 
+    if (list[i].height > 3) {  //check condition of each pokemon height 
       document.write(list[i].name + " (height: " + list[i].height + ')' + ' - wow, that\'s a big Pokemon!' + '<br>'); // loops over each pokemon and if true, prints name, height, and message. with line break
     } else {  
       document.write(list[i].name + " (height: " + list[i].height + ')' + '<br>'); // loops over each pokemon and prints name and heigh. with line break
