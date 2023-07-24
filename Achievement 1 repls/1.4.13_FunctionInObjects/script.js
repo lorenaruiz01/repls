ed6faf96d7bb018eef7878490d7f0446ae9b2124
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-24 14:43:28 -0700

    this keyword definition

--- Achievement 1 repls/1.4.13_FunctionInObjects/script.js
+++ Achievement 1 repls/1.4.13_FunctionInObjects/script.js
@@ -8,3 +8,5 @@ var dog = {
 };
 
 dog.speak();
+
+// The [this] keyword refers to an object, that object which is executing the current bit of javascript code. In other words, the [this] keyword can be used inside an object to refer to the object itself. It is often used in methods to gain access to the object's properties. 
