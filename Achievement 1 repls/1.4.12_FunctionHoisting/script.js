8b04d71f6592a2ff4706732a4a8043a5a8195cbe
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-23 23:16:02 -0700

    add hoisting explanation

--- Achievement 1 repls/1.4.12_FunctionHoisting/script.js
+++ Achievement 1 repls/1.4.12_FunctionHoisting/script.js
@@ -1,3 +1,5 @@
+// When it comes to scope, functions act a bit differently than variables: it doesn’t actually matter where you declare them in your code. This is because of a feature called hoisting. When a program goes to run a JavaScript file, before it executes any of the code, it moves all functions that have been declared using the function keyword to the top of the code—just as if they’d been defined at the beginning of your code file all along.
+
 
 // function is called AFTER its declaration
 function displayHello(){
