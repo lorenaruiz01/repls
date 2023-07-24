3a44994f22e1e377c83454d2f9910228bd5f99fb
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-23 23:14:23 -0700

    script.js

---
+++ Achievement 1 repls/1.4.12_FunctionHoisting/script.js
@@ -0,0 +1,22 @@
+
+// function is called AFTER its declaration
+function displayHello(){
+  console.log("Hello World");
+}
+
+displayHello();
+
+
+//----------------------------
+
+
+// function is called BEFORE its declaration
+displayHi();
+
+function displayHi(){
+  console.log("Hi There");
+}
+
+
+
+
