e0165d35913f690bf20372d3630734a88a50ba66
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-24 22:17:06 -0700

    using this globally

--- Achievement 1 repls/1.4.15_ThisExample-1/script.js
+++ Achievement 1 repls/1.4.15_ThisExample-1/script.js
@@ -7,3 +7,9 @@ let person = {
     } 
        // logs John is 31 years old. 
  person.logInfo();  
+
+
+ // NOTE!
+// So far, you’ve only explored using this in the context of a function above. However, this can also be used outside of a function, for instance, in a “global” context. In the global context (outside of a function), this refers to the global object. If you’re interested in learning more about the use of this, you can take a look at the guide from MDN web docs:
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
