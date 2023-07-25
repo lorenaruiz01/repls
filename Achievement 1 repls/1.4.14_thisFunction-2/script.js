7848ddd4c74e20107e2d4a1a3c602a2df3cfb932
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-24 22:07:06 -0700

    square object

--- Achievement 1 repls/1.4.14_thisFunction-2/script.js
+++ Achievement 1 repls/1.4.14_thisFunction-2/script.js
@@ -11,3 +11,12 @@ dog.speak();
 
 // The [this] keyword refers to an object, that object which is executing the current bit of javascript code. In other words, the [this] keyword can be used inside an object to refer to the object itself. It is often used in methods to gain access to the object's properties. 
 
+//here is another example of when we might use this: 
+
+const square = {
+  side: 4, 
+  length: 5, 
+  perimeter: 20, 
+  area: 25
+}
+
