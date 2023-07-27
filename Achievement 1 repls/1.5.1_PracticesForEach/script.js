14ff9cb4e45744191f86ecb0e252a3a21acd6176
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-27 15:44:51 -0700

    move

--- Achievement 1 repls/1.5.1_PracticesForEach/script.js
+++
@@ -1,5 +0,0 @@
-let array = [1,2,3,4];
-
-array.forEach(function(item,index,array) {
-  console.log("item: " + item + " at index: " + index + " in the array: " + array);
-})
--- Achievement 1 repls/1.5.6_GlobalVariablesAndIIFE/script.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-    function multiply(a, b) {
-        return a * b;
-    }
-
-    console.log(multiply(10,20)); // will result 200
-    document.write(multiply(10,20))
-}());
