57f4ed059545d708eaeee7ff83bddbb3fa9eeb51
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-27 14:00:24 -0700

    move

---
+++ Achievement 1 repls/1.4: JavaScript Functions Part 1/1.4.3_defineAndCallFunction/script.js
@@ -0,0 +1,8 @@
+function ShowMessage() {
+    console.log("Hello Ron!");
+    document.write("Hello Ron!")
+}
+
+ShowMessage();
+
+// In the above example, you can see a function named ShowMessage that displays a message "Hello Ron!" in both the console and the HTML. It’s first defined (with the function keyword), then called (without the function keyword).
