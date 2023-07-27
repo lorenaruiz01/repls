cb8af033f21b73ff71ef27805d71a2fbe880590e
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-26 20:48:40 -0700

    script.js

---
+++ Achievement 1 repls/1.5.5_FunctionComposition/script.js
@@ -0,0 +1,20 @@
+var pug = {
+  age: 4,
+  hasPups: false,
+  name: 'Fred'
+};
+// This code is missing a function. Can you identify the missing function and add it to get a console output?
+
+
+
+function getPupDescription(hasPups) {
+   return hasPups ? 'has pups' : 'has no pups';
+}
+function getDogDescription(dog) {
+   var ageDescription = getAgeDescription(dog.age);
+   var pupDescription = getPupDescription(dog.hasPups);
+
+   return dog.name + ', ' + ageDescription + ', ' + pupDescription;
+}
+
+console.log(getDogDescription(pug));
