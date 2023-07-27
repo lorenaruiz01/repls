1cd877e531c6b2e96dc7d4953ba7bb2428fe4515
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-26 19:57:11 -0700

    add children array to anne array

--- Achievement 1 repls/1.5.2_ObjectKeys/script.js
+++ Achievement 1 repls/1.5.2_ObjectKeys/script.js
@@ -7,3 +7,21 @@ let anne = {
 Object.keys(anne).forEach(function(property) {
   document.write(property + ': ' + anne[property] + '<br>');
 });
+
+anne.children = [
+  {
+    name: 'Sam', 
+    age: 12,
+  },
+  {
+    name: 'Mary',
+    age: 4,
+  }, 
+  {
+    name: 'Max',
+    age: 7
+  }]
+
+  console.log(anne.children);
+
+  
