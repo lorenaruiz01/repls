eb7e2537a6c1fa45b02e1fea5d7c85c277d7e4b4
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-27 15:59:08 -0700

    change file name

---
+++ Achievement 1 repls/1.5: JavaScript Functions Part 2/Exercise-1.5-Challenge-1/script.js
@@ -0,0 +1,41 @@
+// Exercise 1.5: Challenge 1
+//
+// Instructions: In the space indicated, write code that will make sure the variable FOO is of datatype object. Hit the green Run button to check your work in the console.
+
+ 
+const FOO = (function () {
+	// Write your code below this line
+  return {
+    // returns object
+  }
+	// Write your code above this line
+})()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // DO NOT LOOK BELOW THIS LINE
+  if (!FOO) {
+	console.log('FOO is undefined!')
+  } else if (Array.isArray(FOO)){
+	console.log('FOO has a value, but it is not an object, FOO is an array.')
+  } else if (typeof FOO !== 'object'){
+	console.log('FOO has a value, but it is not an object, FOO is a', typeof FOO, '.')
+  }else if (typeof FOO === 'object') {
+	console.log('Yes!  Go you!  FOO is of datatype object!')
+  }
+  
+  
