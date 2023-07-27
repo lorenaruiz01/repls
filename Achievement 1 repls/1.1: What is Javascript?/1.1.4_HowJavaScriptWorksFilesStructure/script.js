e1fb4b52c3554d4ac88fa5e490302fa37e8aa6ff
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-27 12:48:58 -0700

    move

---
+++ Achievement 1 repls/1.1: What is Javascript?/1.1.4_HowJavaScriptWorksFilesStructure/script.js
@@ -0,0 +1,33 @@
+// alert('Hello world');
+
+
+// ================================================
+
+
+// declare variable and assign value
+let myName = 'Bob';
+// print value to html document
+document.write(myName);
+
+// reassign variable with new value
+myName = 'John Doe';
+// print value to html document
+document.write(myName);
+
+// *this will result in 'BobJohnDoe' being printed to the doc
+
+
+// ================================================
+
+
+// let simpleAddition = 2 + 2;
+// document.write(simpleAddition); 
+
+// let size = 100; 
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2; 
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize)
