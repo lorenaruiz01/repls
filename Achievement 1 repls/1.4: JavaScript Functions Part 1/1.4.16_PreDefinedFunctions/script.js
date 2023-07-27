18aecd3cdffbd1855ba6d4e9bc70935298286f3b
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-27 13:59:58 -0700

    move

---
+++ Achievement 1 repls/1.4: JavaScript Functions Part 1/1.4.16_PreDefinedFunctions/script.js
@@ -0,0 +1,83 @@
+
+let a = "hello there";
+let b = "world";
+// console.log(a.concat(b));
+// console.log(a.slice(0));
+// console.log(a.substr(0));
+// console.log(a.split());
+// console.log(a.length);
+// console.log(a.toUpperCase());
+
+// alert("hello");
+// let name = prompt('Please enter your name!');
+// console.log(name);
+let isAccepted = confirm('Do you accept?');
+console.log(isAccepted);
+
+
+
+
+
+
+
+
+
+// 	charAt()
+// Returns the character at the specified index.
+
+// 2	charCodeAt()
+// Returns a number indicating the Unicode value of the character at the given index.
+
+// 3	concat()
+// Combines the text of two strings and returns a new string.
+
+// 4	indexOf()
+// Returns the index within the calling String object of the first occurrence of the specified value, or -1 if not found.
+
+// 5	lastIndexOf()
+// Returns the index within the calling String object of the last occurrence of the specified value, or -1 if not found.
+
+// 6	localeCompare()
+// Returns a number indicating whether a reference string comes before or after or is the same as the given string in sort order.
+
+// 7	length()
+// Returns the length of the string.
+
+// 8	match()
+// Used to match a regular expression against a string.
+
+// 9	replace()
+// Used to find a match between a regular expression and a string, and to replace the matched substring with a new substring.
+
+// 10	search()
+// Executes the search for a match between a regular expression and a specified string.
+
+// 11	slice()
+// Extracts a section of a string and returns a new string.
+
+// 12	split()
+// Splits a String object into an array of strings by separating the string into substrings.
+
+// 13	substr()
+// Returns the characters in a string beginning at the specified location through the specified number of characters.
+
+// 14	substring()
+// Returns the characters in a string between two indexes into the string.
+
+// 15	toLocaleLowerCase()
+// The characters within a string are converted to lower case while respecting the current locale.
+
+// 16	toLocaleUpperCase()
+// The characters within a string are converted to upper case while respecting the current locale.
+
+// 17	toLowerCase()
+// Returns the calling string value converted to lower case.
+
+// 18	toString()
+// Returns a string representing the specified object.
+
+// 19	toUpperCase()
+// Returns the calling string value converted to uppercase.
+
+// 20	valueOf()
+// Returns the primitive value of the specified object.
