6e52700717034e300235f9ccb4a6232f26cf8df3
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-26 20:30:56 -0700

    large function

--- Achievement 1 repls/1.5.4_FunctionComposition-Examples/script.js
+++ Achievement 1 repls/1.5.4_FunctionComposition-Examples/script.js
@@ -13,3 +13,25 @@ let Character = {firstName: 'Homer', lastName: "Simpson"}
 let fullName = getFullName(Character);
 
 console.log(fullName);
+
+
+// function composition is the concept that each function should be as small as possible and that, instead of using one big function, it's better to have many small functions that you can use in combination to obtain the same outcome. 
+
+// here, the function getPersonDescription() is a pure function and does what's expected.  
+
+let anne = {
+  name: 'Anne Smith',
+  age: 38,
+  hasChildren: false
+};
+
+function getPersonDescription(person) {
+  let ageDescription = person.age + ' years old';
+  let childrenDescription = person.hasChildren ? 'has children' : 'has no children';
+
+  return person.name + ', ' + ageDescription + ', ' + childrenDescription;
+}
+
+console.log(getPersonDescription(anne));    // returns: Anne Smith, 38 years old, has no children
+
+// However, there are many action being performed in the function which would be better split into multiple small functions. Doing so woudl make the code more composable, allowing for easy reuse and recombination of its functions:
