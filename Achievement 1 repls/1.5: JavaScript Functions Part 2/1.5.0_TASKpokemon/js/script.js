287fefefb2afd077b68e88f331141723e3129e7c
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-28 14:41:30 -0700

    change typeof syntax

--- Achievement 1 repls/1.5: JavaScript Functions Part 2/1.5.0_TASKpokemon/js/script.js
+++ Achievement 1 repls/1.5: JavaScript Functions Part 2/1.5.0_TASKpokemon/js/script.js
@@ -132,7 +132,7 @@ pokemonRepository.getAll().forEach(function (pokemon) {
 // You may have noticed that the add() function lets you add anything to pokemonList within the repository. You can even add strings or numbers. That’s not good. In a real application, you’d want to make sure pokemonList can only be modified with the correct type of data. Inside the addv function, you can check if the typeof parameter is an object. In combination with a conditional, make sure you can only add the passed argument of the function to pokemonList if it’s an object.
 
 function add (pokemon) {
-  if (pokemon === typeof Object) {
+  if (typeof pokemon === 'object') {
     pokemonList.push(pokemon)
   } else {
     console.log('pokemon cannot be pushed to pokemonList')
