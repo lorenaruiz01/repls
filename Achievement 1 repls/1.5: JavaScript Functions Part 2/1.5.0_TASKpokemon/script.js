aac5907b50f468e742389425169e1768f50a8e20
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-27 21:43:25 -0700

    wrap in IIFE

--- Achievement 1 repls/1.5: JavaScript Functions Part 2/1.5.0_TASKpokemon/script.js
+++ Achievement 1 repls/1.5: JavaScript Functions Part 2/1.5.0_TASKpokemon/script.js
@@ -50,11 +50,54 @@ let pokemonList = [
 
 
 //  TASK Directions:
-// Part 1: forEach() Loops
 
+// Part 1: forEach() Loops
 // Use a forEach() function instead of the for loop you have to iterate over the Pokémon in your pokemonList array in order to print the details of each one. We recommend revisiting and carefully studying the userList example in this Exercise before doing this part of the task. Commit and push the changes to your GitHub repository.
 
 pokemonList.forEach(function (pokemon) {
   console.log(pokemon.name + ' is ' + pokemon.height + ' feet tall.');
 });
 
+
+// Part 2: IIFE
+// 1. In the “scripts.js” file of your project, wrap your pokemonList array in an IIFE to avoid accidentally accessing the global state.
+
+// 2. Before anything else, create a new pokemonRepository variable to hold what your IIFE will return, then assign the IIFE to that variable.
+
+let pokemonRepository = (function () {
+let pokemonList = [
+  {
+    name: 'bulbasaur', 
+    height: 0.7, 
+    types: ['grass', 'poison'],
+  }, 
+  {
+    name: 'charizard', 
+    height: 1.7, 
+    types: ['fire', 'flying'],
+  }, 
+  {
+    name: 'squirtle', 
+    height: 1, 
+    types: ['water'],
+  }
+];
+
+let pokemonList2 = [
+  {
+    name: 'weedle',
+    height: 3,
+    types: ['bug', 'poison'],
+  },
+  {
+    name: 'pikachu',
+    height: 4,
+    types: ['electric'],
+  },
+  {
+    name: 'jigglypuff',
+    height: 5,
+    types:['normal','fairy'],
+  }
+]
+}) ();
