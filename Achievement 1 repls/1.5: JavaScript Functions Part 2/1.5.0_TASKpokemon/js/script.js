1950fb0e9bed452c654713170d9b22a49b89971f
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-30 14:36:43 -0700

    forEach function instead of for loop

--- Achievement 1 repls/1.5: JavaScript Functions Part 2/1.5.0_TASKpokemon/js/script.js
+++ Achievement 1 repls/1.5: JavaScript Functions Part 2/1.5.0_TASKpokemon/js/script.js
@@ -18,13 +18,11 @@ let pokemonList = [
 
   
   function printArrayDetails(list){
-    for (let i = 0; i < list.length; i++){
-      if (list[i].height > 3) {  //check condition of each pokemon height 
-        document.write(list[i].name + " (height: " + list[i].height + ')' + ' - wow, that\'s a big Pokemon!' + '<br>'); // loops over each pokemon and if true, prints name, height, and message. with line break
-      } else {  
-        document.write(list[i].name + " (height: " + list[i].height + ')' + '<br>'); // loops over each pokemon and prints name and height. with line break
-      }
-    }
+    
+    pokemonList.forEach(function (pokemon) {
+      console.log(pokemon.name + ' is ' + pokemon.height + ' feet tall.');
+    });
+
   }
   
   
@@ -32,14 +30,20 @@ let pokemonList = [
   printArrayDetails(pokemonList2);
 
 
+  for (let i = 0; i < list.length; i++){
+    if (list[i].height > 3) {  //check condition of each pokemon height 
+      document.write(list[i].name + " (height: " + list[i].height + ')' + ' - wow, that\'s a big Pokemon!' + '<br>'); // loops over each pokemon and if true, prints name, height, and message. with line break
+    } else {  
+      document.write(list[i].name + " (height: " + list[i].height + ')' + '<br>'); // loops over each pokemon and prints name and height. with line break
+    }
+  }
+
 //  TASK Directions:
 
 // Part 1: forEach() Loops
 // Use a forEach() function instead of the for loop you have to iterate over the Pokémon in your pokemonList array in order to print the details of each one. We recommend revisiting and carefully studying the userList example in this Exercise before doing this part of the task. Commit and push the changes to your GitHub repository.
 
-pokemonList.forEach(function (pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' feet tall.');
-});
+
 
 
 // Part 2: IIFE
