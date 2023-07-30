3107ffd91c3f185a3c07ea7d98c4730b958b13eb
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-30 08:57:55 -0700

    attribute comments

--- Achievement 1 repls/1.6: DOM Interaction/1.6.8_ElementAttribution/script.js
+++ Achievement 1 repls/1.6: DOM Interaction/1.6.8_ElementAttribution/script.js
@@ -1,5 +1,12 @@
 // for an element's attributes, there are a number of different functions you can use:
 
+// 1. getAttribute(attributeName) returns the value assigned to the passed attribute (it will return null if the attribute is absent)
+// 2. hasAttribute(attributeName) returns true or false depending on whether the element has an attribute that matches the passed attribute name
+// 3. setAttribute(attributeName, newValue) assigns what’s passed in newValue to the specified attribute name (if the attribute doesn't exist, it will be added along with the new value)
+
+
+
+
 let container = document.querySelector('.container');
 
 console.log(container.getAttribute('id')); // "main-section"
