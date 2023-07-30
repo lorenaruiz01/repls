3d0d38bff676661bf14381ddc8148b865605c44d
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-30 09:16:34 -0700

    uncomment

--- Achievement 1 repls/1.6: DOM Interaction/1.6.9_classList/script.js
+++ Achievement 1 repls/1.6: DOM Interaction/1.6.9_classList/script.js
@@ -3,7 +3,7 @@ let container = document.querySelector('.container');
 console.log(container.classList.contains('selected')); // false
 
 // uncomment the following code line to add `selected` class notice that there is a css rule that targets `.selected` element and gives them a special style, which means once the class is added, it will be applied
-// container.classList.add('selected');
+container.classList.add('selected');
 
 // uncomment the following line to revert the style, "simulating the effect of unselecting the element visually"
 // container.classList.remove('selected');
