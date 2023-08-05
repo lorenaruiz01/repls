5c6c4b4c4f5b524e22d414112b67895e225c0c42
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-05 16:17:31 -0700

    comment out original code

--- Achievement 1 repls/1.6: DOM Interaction/1.6.12_EventListener/script.js
+++ Achievement 1 repls/1.6: DOM Interaction/1.6.12_EventListener/script.js
@@ -1,17 +1,17 @@
 // Check out this Event Listener repl for some hands-on practice with event listeners! In the example, the button click will toggle between two colors. Try to change the innerText of the button element on-click.
 
-let button = document.querySelector('button');
+// let button = document.querySelector('button');
 
-button.addEventListener('click', function (event) {
-  let target = event.target;
-  target.classList.toggle('button--red');
-  target.classList.toggle('button--green');
+// button.addEventListener('click', function (event) {
+//   let target = event.target;
+//   target.classList.toggle('button--red');
+//   target.classList.toggle('button--green');
   // attempts: 
 
 // button.innerText = 'now i\'m green!';
 // button.innerText = 'now i\'m red!';
 
-});
+// });
 
 // attempts: 
 
@@ -36,7 +36,7 @@ button.addEventListener('click', function (event) {
   } else if (target.classList.contains('button--red')) {
     target.classList.remove('button--red');
     target.classList.add('button--green');
-    button.innerText = "now I'm green";
+    button.innerText = "now I'm green!";
   }
 });
 
