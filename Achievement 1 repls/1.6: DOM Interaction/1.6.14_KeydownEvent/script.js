495d46897e9c8c4d4c6888dd68509fd017e4ffcc
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-06 11:28:27 -0700

    explanation

--- Achievement 1 repls/1.6: DOM Interaction/1.6.14_KeydownEvent/script.js
+++ Achievement 1 repls/1.6: DOM Interaction/1.6.14_KeydownEvent/script.js
@@ -15,3 +15,5 @@ window.addEventListener('keydown', function(event) { ... });
 let survey_form = document.querySelector('#survey_form');
 // This line of code uses the document.querySelector() method to select an HTML element with the ID "survey_form". The selected element is assigned to the variable survey_form. This assumes that there is an HTML element with the ID "survey_form" in the document.
 
+let isFormHidden = survey_form.classList.contains('hidden');
+// This line of code checks whether the survey_form element has a CSS class named "hidden". The .classList.contains() method is used to determine if the class is present on the element. The result (true or false) is stored in the variable isFormHidden.
