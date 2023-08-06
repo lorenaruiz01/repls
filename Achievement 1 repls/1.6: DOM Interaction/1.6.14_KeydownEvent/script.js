67d7bde5b81fc1fa4bc646da8f4a7b89223d898f
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-06 11:28:00 -0700

    explanation

--- Achievement 1 repls/1.6: DOM Interaction/1.6.14_KeydownEvent/script.js
+++ Achievement 1 repls/1.6: DOM Interaction/1.6.14_KeydownEvent/script.js
@@ -9,5 +9,6 @@ window.addEventListener('keydown', function(event){
 // explanation by chatgpt:
 // This code snippet is written in JavaScript and adds an event listener to the window object. The event being listened for is the "keydown" event, which occurs when a key on the keyboard is pressed down. Let's break down what this code does step by step:
 
-
+window.addEventListener('keydown', function(event) { ... });
+// This line of code attaches an event listener to the window object. It listens for the "keydown" event, which is triggered when any key on the keyboard is pressed. When this event occurs, the provided function is executed.
 
