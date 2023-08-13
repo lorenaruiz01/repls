ce5ae3104792000e784578b250520c0f936f3f96
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-13 15:44:15 -0700

    create empty array for pokemonList

--- Achievement 1 repls/1.7: APIs, Ajax & Asynchronous Behavior/1.7.0: POKEMON TASK/js/script.js
+++ Achievement 1 repls/1.7: APIs, Ajax & Asynchronous Behavior/1.7.0: POKEMON TASK/js/script.js
@@ -1,21 +1,6 @@
 let pokemonRepository = (function () {    // wrapping pokemonList array inside of an IIFE (Immediately Invoked Function Expression)
-    let pokemonList = [                     // an array of objects
-      {
-        name: 'bulbasaur', 
-        height: 0.7, 
-        types: ['grass', 'poison'],
-      }, 
-      {
-        name: 'charizard', 
-        height: 1.7, 
-        types: ['fire', 'flying'],
-      }, 
-      {
-        name: 'squirtle', 
-        height: 1, 
-        types: ['water'],
-      }
-    ];
+    let pokemonList = [];                     // an array of objects
+
   
     function add (pokemon) {    // the add function adds the selected pokemon to the array
       if (typeof pokemon === 'object' &&    // checks whether pokemon is an object
