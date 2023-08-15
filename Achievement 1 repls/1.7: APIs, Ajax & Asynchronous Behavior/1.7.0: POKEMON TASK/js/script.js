1dccc4502e0a7b0a2aba6d4eec8ab9fc2ff363d8
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-14 21:12:14 -0700

    correction

--- Achievement 1 repls/1.7: APIs, Ajax & Asynchronous Behavior/1.7.0: POKEMON TASK/js/script.js
+++ Achievement 1 repls/1.7: APIs, Ajax & Asynchronous Behavior/1.7.0: POKEMON TASK/js/script.js
@@ -87,7 +87,7 @@ let pokemonRepository = (function () {    // wrapping pokemonList array inside o
         let modal = document.createElement('div');                      // creates new div to hold modal
         modal.classList.add('modal');                                   // adds class of modal to newly created div
 
-        let closeButtonElement = document.createElement('butto');       // create a button element
+        let closeButtonElement = document.createElement('button');      // create a button element
         closeButtonElement.classList.add('modal-close');                // add class of modal-close to newly created button
         closebuttonElememt.innerText = 'X';                             // set inner text of button to 'X'
         closeButtonElement.addEventListener('click', hideModal);        // hide modal when user clicks button    
