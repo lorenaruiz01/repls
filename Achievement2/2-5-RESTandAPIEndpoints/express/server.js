4f2460c37eaedc560c1a294f7ff8819d394deed3
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-10-07 16:49:00 -0700

    edit with prettier

--- Achievement2/2-5-RESTandAPIEndpoints/express/server.js
+++ Achievement2/2-5-RESTandAPIEndpoints/express/server.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // app represents the server
 
 // set up endpoint
-app.get('/', (request, response) => {
-    response.send('hello world!');
-})
-// get() represents HTTP method. get() method takes two arguments. The first argument is a string representing the endpoint. The second argument is a funciton that represents what is going to happen with the request and response objects. 
+app.get("/", (request, response) => {
+  response.send("hello world!");
+});
+// get() represents HTTP method. get() method takes two arguments. The first argument is a string representing the endpoint. The second argument is a funciton that represents what is going to happen with the request and response objects.
 
-
-app.listen(8080, () => console.log("listening on 8080"))
+app.listen(8080, () => console.log("listening on 8080"));
 // listen() method takes two arguments. The first argument is a number that represents the port on which the server will be listening. The second argument is a function that is going to be called when the server starts listening.
