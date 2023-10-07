a948aad88261838fd7fd935a847f2ba36951c840
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-10-07 15:14:04 -0700

    comment

--- Achievement2/2-5-RESTandAPIEndpoints/express/server.js
+++ Achievement2/2-5-RESTandAPIEndpoints/express/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 // app represents the server
 
+
+
+
+// listen() method takes two arguments. The first argument is a number that represents the port on which the server will be listening. The second argument is a function that is going to be called when the server starts listening.
 app.listen(8080, () => console.log("listening on 8080"))
