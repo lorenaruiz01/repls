8855fde8f608073a22674ace048b1d6189a81078
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-10-07 15:19:28 -0700

    set up endpoint with fxn on req res objects

--- Achievement2/2-5-RESTandAPIEndpoints/express/server.js
+++ Achievement2/2-5-RESTandAPIEndpoints/express/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 // app represents the server
 
+// set up endpoint
+app.get('/', )
+// get() represents HTTP method. get() method takes two arguments. The first argument is a string representing the endpoint. The second argument is a funciton that represents what is going to happen with the request and response objects. 
 
 
-
-// listen() method takes two arguments. The first argument is a number that represents the port on which the server will be listening. The second argument is a function that is going to be called when the server starts listening.
 app.listen(8080, () => console.log("listening on 8080"))
+// listen() method takes two arguments. The first argument is a number that represents the port on which the server will be listening. The second argument is a function that is going to be called when the server starts listening.
