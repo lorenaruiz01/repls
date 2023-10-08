5177247dd3cc8e94a829fe9705832b737564c6fa
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-10-07 17:17:39 -0700

    require express, body-parser, and uuid

--- Achievement2/2-5-RESTandAPIEndpoints/test/test.js
+++ Achievement2/2-5-RESTandAPIEndpoints/test/test.js
@@ -1,2 +1,11 @@
 
-console.log('testing testing');
+// console.log('testing testing');
+
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
+
+const app = express();
+
+app.use(bodyParser.json());
+
