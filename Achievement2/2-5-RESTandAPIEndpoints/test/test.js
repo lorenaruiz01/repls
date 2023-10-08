b5783cbcf8da2c163ef4e1def52a2667ba3b5c98
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-10-07 17:18:05 -0700

    add list of students

--- Achievement2/2-5-RESTandAPIEndpoints/test/test.js
+++ Achievement2/2-5-RESTandAPIEndpoints/test/test.js
@@ -9,3 +9,29 @@ const app = express();
 
 app.use(bodyParser.json());
 
+let students = [
+  {
+    id: 1,
+    name: 'Jessica Drake',
+    classes: {
+      biology: 95,
+      algebra: 92
+    }
+  },
+  {
+    id: 2,
+    name: 'Ben Cohen',
+    classes: {
+      biology: 95,
+      algebra: 92
+    }
+  },
+  {
+    id: 3,
+    name: 'Lisa Downing',
+    classes: {
+      biology: 95,
+      algebra: 92
+    }
+  }
+];
